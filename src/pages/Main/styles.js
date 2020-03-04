a31a8089fae8d0d3b60b6ec1e3d9b2a2b9e37e86
yago-codev <yagomilano92@gmail.com>
2020-03-04 02:40:01 -0300

    estilos globais

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,12 +1,3 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
-  font-size: 24px;
-  color: ${props => (props.error ? 'red' : '#7159c1')};
-  font-family: Arial, Helvetica, sans-serif;
-
-  small {
-    font-size: 16px;
-    color: #7159c1;
-  }
-`;
+export const Title = styled.h1``;
