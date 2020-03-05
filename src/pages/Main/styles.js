2769a550507e398a1d7cf1864255824e2493165f
yago-codev <yagomilano92@gmail.com>
2020-03-04 23:12:02 -0300

    Criando os componentes e estilização da página Main

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,3 +1,73 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1``;
+export const Container = styled.div`
+  max-width: 700px;
+  background: #131419;
+  border-radius: 4px;
+  box-shadow: -5px -5px 10px rgba(255, 255, 255, 0.05),
+    5px 5px 15px rgba(0, 0, 0, 0.5);
+  padding: 30px;
+  margin: 80px auto;
+
+  h1 {
+    font-size: 20px;
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+  }
+
+  svg {
+    margin-right: 10px;
+  }
+`;
+
+export const Form = styled.form`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: row;
+
+  input {
+    flex: 1;
+    height: 50px;
+    background: #131419;
+    border: none;
+    outline: none;
+    border-radius: 40px;
+    padding: 5px 15px;
+    color: #03a9f4;
+    font-size: 18px;
+    box-shadow: inset -2px -2px 6px rgba(255, 255, 255, 0.1),
+      inset 2px 2px 6px rgba(0, 0, 0, 0.8);
+  }
+`;
+
+export const SubmitButton = styled.button.attrs({
+  type: 'submit',
+})`
+  width: 50px;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #131419;
+  margin-left: 15px;
+  border-radius: 50%;
+  border: none;
+  box-shadow: -2px -2px 6px rgba(255, 255, 255, 0.1),
+    2px 2px 6px rgba(0, 0, 0, 0.8);
+
+  svg {
+    margin-right: 0;
+    fill: #c7c7c7;
+  }
+
+  :active {
+    box-shadow: inset -2px -2px 6px rgba(255, 255, 255, 0.1),
+      inset 2px 2px 6px rgba(0, 0, 0, 0.8);
+
+    svg {
+      fill: #006c9c;
+      margin-right: 0;
+    }
+  }
+`;
